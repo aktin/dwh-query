f6d49285938c2c2ad84dbe18f67ecd59aedd213f
rwm <raphael@majeed.de>
2016-05-03 17:36:41 +0200

    copy resources

--- wolfsburg-monthly-report/src/main/java/org/aktin/report/wolfsburg/WolfsburgMonthly.java
+++ wolfsburg-monthly-report/src/main/java/org/aktin/report/wolfsburg/WolfsburgMonthly.java
@@ -2,6 +2,7 @@ package org.aktin.report.wolfsburg;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.aktin.report.Report;
@@ -26,12 +27,28 @@ public class WolfsburgMonthly implements Report {
 		return null;
 	}
 
+
+	private void copyResources(String[] names, String resourcePrefix, Path workingDirectory) throws IOException{
+		for( String name : names ){
+			try( InputStream in = getClass().getResourceAsStream(resourcePrefix+name) ){
+				Files.copy(in, workingDirectory.resolve(name));				
+			}
+		}		
+	}
+	
 	@Override
 	public String[] copyResourcesForR(Path workingDirectory) throws IOException {
+		String[] resNames = {"generate-report-resources-mod.R"};
+		String resPrefix = "/";
+		copyResources(resNames, resPrefix, workingDirectory);
+		// return resource names within workingDirectory. 
+		// do not include path separators or sub directories.
+		return resNames;
+		
 		//ToDo put RScript in workingDirectory
 		//getClass().getResource(name);
-		String [] Rfiles = {"C:\\temp\\RScript-Test\\generate-report-resources-mod.R"};
-		return Rfiles;
+		//String [] Rfiles = {"C:\\temp\\RScript-Test\\generate-report-resources-mod.R"};
+		//return Rfiles;
 	}
 
 	@Override
---
+++ wolfsburg-monthly-report/src/test/java/org/aktin/report/wolfsburg/TestCopyResources.java
@@ -0,0 +1,38 @@
+package org.aktin.report.wolfsburg;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.Test;
+
+/**
+ * Tests copying of resources
+ * @author R.W.Majeed
+ *
+ */
+public class TestCopyResources {
+	/**
+	 * Verifies that all resources are available, readable
+	 * and can be copied to the target path. Also verifies
+	 * that no additional files are copied.
+	 * 
+	 * @throws IOException if anything fails
+	 */
+	@Test
+	public void verifyResourcesCopied_R() throws IOException{
+		WolfsburgMonthly m = new WolfsburgMonthly();
+		Path temp = Files.createTempDirectory("test-res-copy");
+		// should not throw any exception
+		String[] names = m.copyResourcesForR(temp);
+
+		// remove copied files
+		for( String name : names ){
+			Files.delete(temp.resolve(name));
+		}
+		// remove temp directory
+		// will also verify that no other resources were copied
+		// since directory needs to be empty for delete
+		Files.delete(temp);
+	}
+}

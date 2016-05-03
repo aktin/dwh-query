04ea5997f76b3f95f0780233e12067ea1a69cbd8
rwm <raphael@majeed.de>
2016-05-03 17:16:21 +0200

    use try-with-resources. Specific exceptions and not nested if possible.

--- report-manager/src/main/java/org/aktin/report/manager/PDFGenerator.java
+++ report-manager/src/main/java/org/aktin/report/manager/PDFGenerator.java
@@ -7,6 +7,7 @@ import java.io.OutputStream;
 import java.nio.file.Path;
 
 import org.apache.fop.apps.FopFactory;
+import org.apache.fop.apps.FOPException;
 import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.MimeConstants;
 
@@ -24,7 +25,6 @@ import javax.xml.transform.stream.StreamSource;
 import org.aktin.report.Report;
 import org.xml.sax.SAXException;
 
-import com.sun.org.apache.xml.internal.utils.URI;
 
 /**
  * Generates a Report from the Rscript output files
@@ -39,7 +39,7 @@ import com.sun.org.apache.xml.internal.utils.URI;
  *
  */
 class PDFGenerator {
-
+	//private static final Logger log = Logger.getLogger(PDFGenerator.class.getName());	
 	
 	/**
 	 * Generate a PDF report. After return of the function, no additional files
@@ -53,7 +53,7 @@ class PDFGenerator {
 	 */
 	//ToDo: SVG/Image-Problems when built via maven?
 	//http://stackoverflow.com/questions/31386864/apache-fop-2-0-why-is-no-imagepreloader-found-for-svg
-	public void generatePDF(Path workingPath, Report report, Path pdf) throws IOException{
+	public void generatePDF(Path workingPath, Report report, Path pdf) throws IOException {
 		String[] files = report.copyResourcesForFOP(workingPath);
 		// invoke Apache FOP
 		// clean up afterwards
@@ -65,50 +65,46 @@ class PDFGenerator {
 		// (reuse if you plan to render multiple documents!)
 		//Config File: https://xmlgraphics.apache.org/fop/2.0/configuration.html
 		FopFactory fopFactory;
+		
+		//svgs tend to cause path problems if the baseDir is not set correctly
 		try {
-			//svgs tend to cause path problems if the baseDir is not set correctly
-			fopFactory = FopFactory.newInstance(workingPath.resolve("dummy.xconf").toFile()); //ToDo this should maybe be included in the Report Interface to allow for configuration
-			
-			// Step 2: Set up output stream.
-			// Note: Using BufferedOutputStream for performance reasons (helpful with FileOutputStreams).
-			//OutputStream out = new BufferedOutputStream(new FileOutputStream(new File("C:\\temp\\myfile.pdf")));
-			//OutputStream out = new BufferedOutputStream(new FileOutputStream(workingPath.resolve("myfile.pdf").toFile()));
-			OutputStream out = new BufferedOutputStream(new FileOutputStream(pdf.toFile()));
-
-			try {
-			    // Step 3: Construct fop with desired output format
-			    Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, out);			   
-
-			    // Step 4: Setup JAXP using identity transformer
-			    TransformerFactory factory = TransformerFactory.newInstance();
-			    //Transformer transformer = factory.newTransformer(); // identity transformer
-			    Transformer transformer = factory.newTransformer(new StreamSource(new File(files[1]))); //Second file from Report interface is the XSL file
-
-			    // Step 5: Setup input and output for XSLT transformation
-			    // Setup input stream
-			    Source src = new StreamSource(new File(files[0])); //First file from Report interface is the XML input (Source)
-
-			    // Resulting SAX events (the generated FO) must be piped through to FOP
-			    Result res = new SAXResult(fop.getDefaultHandler());
-
-			    // Step 6: Start XSLT transformation and FOP processing
-			    transformer.transform(src, res);
-
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally {
-			    //Clean-up
-			    out.close();
-			}
-		} catch (SAXException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}  
-
+			fopFactory = FopFactory.newInstance(workingPath.resolve("dummy.xconf").toFile());
+		} catch (SAXException e) {
+			throw new IOException("FOP initialisation failed", e);
+		} 
+		//ToDo this should maybe be included in the Report Interface to allow for configuration
 		
-		
-			
+		// Step 2: Set up output stream.
+		// Note: Using BufferedOutputStream for performance reasons (helpful with FileOutputStreams).
+		//OutputStream out = new BufferedOutputStream(new FileOutputStream(new File("C:\\temp\\myfile.pdf")));
+		//OutputStream out = new BufferedOutputStream(new FileOutputStream(workingPath.resolve("myfile.pdf").toFile()));
+		// try-with-resource will be closed automatically
+		try( OutputStream out = new BufferedOutputStream(new FileOutputStream(pdf.toFile())) ){
+		    // Step 3: Construct fop with desired output format
+		    Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, out);			   
+
+		    // Step 4: Setup JAXP using identity transformer
+		    TransformerFactory factory = TransformerFactory.newInstance();
+		    // configuration of transformer factory
+		    //Transformer transformer = factory.newTransformer(); // identity transformer
+		    Transformer transformer = factory.newTransformer(new StreamSource(new File(files[1]))); //Second file from Report interface is the XSL file
+
+		    // Step 5: Setup input and output for XSLT transformation
+		    // Setup input stream
+		    Source src = new StreamSource(new File(files[0])); //First file from Report interface is the XML input (Source)
+
+		    // Resulting SAX events (the generated FO) must be piped through to FOP
+		    Result res = new SAXResult(fop.getDefaultHandler());
+
+		    // Step 6: Start XSLT transformation and FOP processing
+		    transformer.transform(src, res);
+
+		} catch (TransformerException e) {
+			throw new IOException("Transformation error", e);
+		} catch (FOPException e) {
+			throw new IOException("FOP processing failed", e);
+		}
+
 		//throw new UnsupportedOperationException("TODO implement");
 	}
 	

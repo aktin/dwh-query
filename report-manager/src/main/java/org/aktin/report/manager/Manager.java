e1dc252fe6b3fcd8aa38a5364d6b15159b667818
R.W.Majeed <raphael@majeed.de>
2016-07-12 23:44:04 +0200

    first implementation of manager

---
+++ report-manager/src/main/java/org/aktin/report/manager/Manager.java
@@ -0,0 +1,26 @@
+package org.aktin.report.manager;
+
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.aktin.Module;
+import org.aktin.Preference;
+import org.aktin.report.Report;
+
+@Singleton
+//@Preferences(group="reports")
+public class Manager extends Module{
+	public static final String PREF_RSCRIPT = "reports.rScript";
+	
+	@Inject @Any
+	Instance<Report> reports;
+	
+	@Preference(id=PREF_RSCRIPT)
+	public void setRScript(String path){
+		
+	}
+	
+	
+}

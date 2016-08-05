8070c5529124b1198d7315a8ac4ea0d6b211f1d4
Volker Thiemann <volker.thiemann@uni-oldenburg.de>
2016-08-05 14:06:34 +0200

    export descriptor und demo-eav auf aktuelles CDA-Release angepasst

--- report-demo/data/generate-report-resources-mod.R
+++ report-demo/data/generate-report-resources-mod.R
@@ -27,9 +27,10 @@ df$age = floor(difftime(df$admit.ts,df$dob)/365.25)
 
 
 # remove prefixes from string values
-df$triage.result = as.fadfctor(substring(tmp$triage, first=5))
+df$triage.result = as.factor(substring(tmp$triage, first=5))
 df$cedis = as.factor(substring(tmp$cedis, first=9))
-df$diagnosis = as.factor(substring(tmp$Diagnose, first=9, last =11))
+df$diagnosis = as.factor(substring(tmp$Diagnose, first=9, last =11)) #ICD10 Codes Category (3-char only)
+df$discharge = as.factor(substring(tmp$entlassung.1, first=7))
 # TODO more columns
 
 
@@ -43,6 +44,12 @@ df$admit.wd <- factor(x=strftime(df$admit.ts,format="%a"), levels=weekday.levels
 # Hour of admission
 hour.levels = sprintf('%02i',0:23)
 df$admit.h <- factor(x=strftime(df$admit.ts,format="%H"), levels=hour.levels, ordered=TRUE)
+
+#Hour of admission per weekday
+admit.hwd <- matrix(,7,24)
+for(i in 1:length(weekday.levels)) {
+  admit.hwd[i,] <- as.vector(table(factor(x=strftime(df$admit.ts[df$admit.wd == weekday.levels[i]],format="%H"), levels=hour.levels, ordered=TRUE)))
+}
 
 # Hour of discharge
 df$discharge.h <- factor(x=strftime(df$discharge.ts,format="%H"), levels=hour.levels, ordered=TRUE)
@@ -65,6 +72,12 @@ df$therapy.d <- df$therapy.ts - df$admit.ts
 df$therapy.d[df$therapy.d < 0] <- NA
 df$therapy.d[df$therapy.d > 24*60] <- NA
 
+# Time to discharge
+df$discharge.d <- df$discharge.ts - df$admit.ts
+# Values out of bounds (<0h or >24h) => NA
+df$discharge.d[df$discharge.d < 0] <- NA
+df$discharge.d[df$discharge.d > 24*60] <- NA     #could be more than 24 hours!
+
 # Referral Codes
 df$referral <- factor(x=tmp$zuweisung)
 
@@ -77,21 +90,26 @@ df$cedis <- factor(x=tmp$cedis,t(cedis[1]))
 
 # XHTML tables
 source('xhtml-table.R')
-xml.dir <- '../target/'
+#xml.dir <- '../target/'
+xml.dir <- '/'
 
 # Graphics & Plots
 #todo: main/sub Überschriften sinnvoll setzen und x/ylab ggf. anpassen
 library(lattice)
 
-gfx.dir <- '../target/'
+#gfx.dir <- '../target/'
+gfx.dir <- '/'
 gfx.ext <- '.svg'
 gfx.dev <- 'svg'
 
+mycolors <- c("red","green","blue")
+
 # Counts per Hour
-graph <- barchart(table(df$admit.h), horizontal=FALSE, xlab="Uhrzeit (Stunde)", ylab="Anzahl (n)")
+graph <- barchart(table(df$admit.h), horizontal=FALSE, xlab="Uhrzeit (Stunde)", ylab="Anzahl (n)",col=mycolors, auto.key=TRUE, par.settings =list(superpose.polygon=list(col=mycolors)))
 # for viewing: print(graph)
 # Save as SVG file
 trellis.device(gfx.dev,file=paste0(gfx.dir,'admit.h',gfx.ext), width=8,height=4)
+#trellis.par.set(list(add.line = list(col = "red")) )
 print(graph)
 dev.off()
 # Write table
@@ -103,6 +121,21 @@ graph <- barchart(table(df$admit.wd), horizontal=FALSE, xlab="Wochentag", ylab="
 trellis.device('svg',file=paste0(gfx.dir,'admit.wd',gfx.ext),width=8,height=4)
 print(graph)
 dev.off()
+
+# Counts per Hour/Weekday
+colors <- rainbow(length(weekday.levels)) 
+graph <- plot(admit.hwd[1,], xlab="Uhrzeit (Stunde)", ylab="Anzahl (n)")   #ToDo: How To Plot a Matrix?
+for (i in 1:length(weekday.levels)) {
+  lines(admit.hwd[i,],type="b",col=colors[i])
+}
+legend('topleft',1:length(weekday.levels), legend=weekday.levels, cex=0.8, col=colors, title="Tage")
+trellis.device('svg',file=paste0(gfx.dir,'admit.hwd',gfx.ext),width=8,height=4)
+print(graph)
+dev.off()
+
+#Transport and referral
+xhtml.table(table(df$transport,useNA = "always"), file=paste0(xml.dir,'transport.xml'))
+xhtml.table(table(df$referral,useNA = "always"), file=paste0(xml.dir,'referral.xml'))
 
 # Time to physician
 #graph <- barchart(table(df$phys.d), horizontal=FALSE, xlab="Zeit von Aufnahme bis Arztkontakt in Minuten")
@@ -124,7 +157,6 @@ trellis.device('svg',file=paste0(gfx.dir,'therapy.d.hist',gfx.ext),width=8,heigh
 print(graph)
 dev.off()
 
-
 # Time to physician mean grouped by triage result
 x <- aggregate(x=list(avg=df$phys.d), by=list(triage=df$triage.result), FUN=mean)
 graph <- barchart(avg ~ triage, data=x, horizontal=FALSE, ylab="Durchschn. Zeit bis Arztkontakt", xlab="Triage")
@@ -132,6 +164,15 @@ trellis.device('svg',file=paste0(gfx.dir,'triage.phys.d.avg',gfx.ext),width=8,he
 print(graph)
 dev.off()
 
+# Time to discharge
+graph <- histogram(as.numeric(df$discharge.d,unit='mins'),xlab="Zeit von Aufnahme bis zur Entlassung/Verlegung in Minuten")
+trellis.device('svg',file=paste0(gfx.dir,'discharge.d.hist',gfx.ext),width=8,height=4)
+print(graph)
+dev.off()
+
+output <- c('','intern','extern','Fallzahl','1000','500','Mittelwert','120','180')
+dim(output) <- c(3,3)
+xhtml.table(output, file=paste0(xml.dir,'discharge.d.xml'))
 
 # A little more sophisticated: Table with many aggregate functions
 # list of aggregate functions we want to apply
@@ -140,6 +181,14 @@ agg.list <- lapply(agg.funs, function(fun){aggregate(x=df$phys.d, by=list(triage
 x <- data.frame(triage=levels(df$triage.result), agg.list)
 rm(agg.funs, agg.list)
 xhtml.table(x, file=paste0(xml.dir,'triage.phys.d.xml'))
+
+#Entlassung & Co
+graph <- barchart( table(df$discharge), xlab="Entlassung & Verlegung (Häufigkeit)")
+trellis.device('svg',file=paste0(gfx.dir,'discharge',gfx.ext),width=8,height=4)
+print(graph)
+dev.off()
+
+xhtml.table(table(df$discharge,useNA = "always"), file=paste0(xml.dir,'discharge.xml'))
 
 #TOP10 CEDIS
 t <- table(df$cedis) #frequencies
@@ -152,7 +201,9 @@ dev.off()
 #CEDIS Groups
 #HOW can I map the CEDIS levels? 001-012=CV, 051-056=HNE,...
 #This causes warnings, but it works :)
-df$cedis <- factor(x=tmp$cedis,t(cedis[1]),labels=t(cedis[2]))
+df$cedis <- factor(x=tmp$cedis,t(cedis[1]),labels=t(cedis[2]))  #map Categories
+df$cedis <- factor(x=df$cedis,t(cedis[1]),labels=t(cedis[3]))   #map Labels
+df$cedis <- droplevels(df$cedis) #not ideal, only showing used categories
 graph <- barchart(df$cedis,xlab="CEDIS Vorstellungsgründe nach Gruppen (Häufigkeit)")
 trellis.device('svg',file=paste0(gfx.dir,'cedis_groups',gfx.ext),width=8,height=4)
 print(graph)

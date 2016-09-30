a2ff672d71c1501e9263a043ff93b439c2960898
R.W.Majeed <raphael@majeed.de>
2016-09-30 18:42:42 +0200

    constructors for manual instantiation

--- report-manager/src/main/java/org/aktin/report/manager/ReportManager.java
+++ report-manager/src/main/java/org/aktin/report/manager/ReportManager.java
@@ -1,5 +1,8 @@
 package org.aktin.report.manager;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.spi.AfterBeanDiscovery;
@@ -8,6 +11,7 @@ import javax.inject.Singleton;
 
 import org.aktin.Module;
 import org.aktin.Preference;
+import org.aktin.dwh.PreferenceKey;
 import org.aktin.report.Report;
 
 /**
@@ -26,17 +30,32 @@ import org.aktin.report.Report;
 @Singleton
 //@Preferences(group="reports")
 public class ReportManager extends Module{
-	public static final String PREF_RSCRIPT = "reports.rScript";
-	
 	@Inject @Any
-	Instance<Report> reports;
+	Instance<Report> cdiReports;
+	private Report[] staticReports;
 	
-	@Preference(id=PREF_RSCRIPT)
-	public void setRScript(String path){
+	@Inject @Preference(key=PreferenceKey.i2b2Project)
+	String rScript;
+	
+
+	/**
+	 * Empty constructor for CDI
+	 */
+	protected ReportManager(){
 	}
-	
+	public ReportManager(String rScript, Report...reports){
+		this.rScript = rScript;
+		this.staticReports = reports;
+		
+	}
 	public Iterable<Report> reports(){
-		return reports;
+		if( cdiReports != null ) {
+			return cdiReports;
+		}else if( staticReports != null ){
+			return Arrays.asList(staticReports);
+		}else{
+			return Collections.emptyList();
+		}
 	}
 	
 }

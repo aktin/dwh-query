a3e20608f81b247acbd86e30e6a272a9943d318c
R.W.Majeed <raphael@majeed.de>
2016-10-04 23:20:09 +0200

    prevent output to stdout

--- report-manager/src/test/resources/demo.R
+++ report-manager/src/test/resources/demo.R
@@ -1,5 +1,6 @@
 library(lattice)
+x <- 1:10
 graph <- barchart(runif(10) ~ x, horizontal=FALSE)
 trellis.device('svg',file='barchart1.svg',width=8, height=4)
 print(graph)
-dev.off()
+d <- dev.off()

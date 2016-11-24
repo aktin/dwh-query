4f50a204b27c4a742f709978fc6d49b48765d806
Volker Thiemann <volker.thiemann@uni-oldenburg.de>
2016-11-24 11:59:57 +0100

    Timestamp Revision in DQ-Export

--- reports/cda-field-stats/src/main/resources/concept_count.sql
+++ reports/cda-field-stats/src/main/resources/concept_count.sql
@@ -5,4 +5,14 @@ union
 select 'SEX:FEMALE' as concept_cd, '@' as modifier_cd, count(distinct f.encounter_num) from "i2b2crcdata"."patient_dimension" as p, "i2b2crcdata"."observation_fact" as f where p.patient_num = f.patient_num and p.sex_cd = 'F'
 union
 select 'AKTIN:ADMISSIONTIME' as concept_cd, '@' as modifier_cd, count(start_date) from "i2b2crcdata"."visit_dimension"
+union
+select 'AKTIN:DISCHARGETIME' as concept_cd, '@' as modifier_cd, count(end_date) from "i2b2crcdata"."visit_dimension"
+union
+select distinct concept_cd, modifier_cd, count (distinct encounter_num) from "i2b2crcdata"."observation_fact" where modifier_cd = 'effectiveTime' and concept_cd in ('LOINC:26436-6','LOINC:18767-4', 'LOINC:50556-0', 'LOINC:34534-8', 'LOINC:25061-3', 'LOINC:42148-7', 'LOINC:24725-4', 'LOINC:25045-6', 'LOINC:46305-9','LOINC:38008-9', 'LOINC:30745-4', 'LOINC:28561-9', 'LOINC:37637-6', 'LOINC:43468-8', 'LOINC:25056-3') group by concept_cd, modifier_cd
+union
+select 'MTS', modifier_cd, count (distinct encounter_num) from "i2b2crcdata"."observation_fact" where modifier_cd = 'effectiveTimeLow' and concept_cd like 'MTS:%'  group by modifier_cd
+union
+select 'ESI', modifier_cd, count (distinct encounter_num) from "i2b2crcdata"."observation_fact" where modifier_cd = 'effectiveTimeLow' and concept_cd like 'ESI:%'  group by modifier_cd
+union
+select concept_cd, modifier_cd, count (distinct encounter_num) from "i2b2crcdata"."observation_fact" where modifier_cd = 'effectiveTimeLow' and concept_cd = 'AKTIN:ASSESSMENT'  group by concept_cd, modifier_cd
 order by concept_cd asc

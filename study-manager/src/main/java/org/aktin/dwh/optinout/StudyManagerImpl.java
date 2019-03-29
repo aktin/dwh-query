ec55eb95e2fb1851d469501fd1ca20d506923b9c
R.W.Majeed <raphael@majeed.de>
2019-03-29 19:38:31 +0100

    prepare study manager synchronization with i2b2

--- study-manager/src/main/java/org/aktin/dwh/optinout/StudyManagerImpl.java
+++ study-manager/src/main/java/org/aktin/dwh/optinout/StudyManagerImpl.java
@@ -2,16 +2,22 @@ package org.aktin.dwh.optinout;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.Executor;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.naming.InitialContext;
@@ -23,6 +29,8 @@ import org.aktin.dwh.Anonymizer;
 import org.aktin.dwh.PreferenceKey;
 import org.aktin.dwh.optinout.sic.CodeGeneratorFactory;
 
+import de.sekmi.histream.ext.Patient;
+
 
 @Singleton
 public class StudyManagerImpl implements StudyManager {
@@ -32,6 +40,7 @@ public class StudyManagerImpl implements StudyManager {
 	private CodeGeneratorFactory codeFactory;
 	private Anonymizer anon;
 	private Preferences prefs;
+	private Executor executor; // will be filled via resource injection or setter method
 
 	// TODO memory cache of pat_ref,pat_psn to use for lookup during CDAImport
 	
@@ -41,7 +50,7 @@ public class StudyManagerImpl implements StudyManager {
 
 	/**
 	 * Set the preference interface.
-	 * This class reads the following properties: {@link PreferenceKey#i2b2DatasourceCRC}
+	 * This class reads the following properties: {@link PreferenceKey#i2b2DatasourceCRC}, {@link PreferenceKey#i2b2Project} for patient/visit linking via i2b2.patient_ide/encounter_ide.
 	 * @param prefs preferences
 	 */
 	@Inject
@@ -58,6 +67,16 @@ public class StudyManagerImpl implements StudyManager {
 	 */
 	public void setDataSource(DataSource ds){
 		this.ds = ds;
+	}
+
+	/**
+	 * Set the executor service for running asynchronous operations.
+	 * E.g. syncBatch
+	 * @param executor executor service
+	 */
+	@Resource
+	public void setExecutor(ManagedExecutorService executor){
+		this.executor = executor;
 	}
 
 	private void initializeDatabase() throws NamingException, SQLException, IOException {
@@ -229,20 +248,108 @@ public class StudyManagerImpl implements StudyManager {
 		return cf;
 	}
 	
+	private PreparedStatement prepareLastVisitUpdate(Connection dbc, Study study) throws SQLException {
+		return null;
+	}
+	
+	private PreparedStatement prepareSyncStatement(Connection dbc, PatientReference ref, Study study, boolean skipLinked, String idRoot, String idExt) throws SQLException {
+		Objects.requireNonNull(dbc);
+		Objects.requireNonNull(ref);
+		Objects.requireNonNull(prefs);
+		ArrayList<String> args = new ArrayList<>();
+		StringBuilder sql = new StringBuilder();
+		switch( ref ) {
+		case Patient:
+			sql.append("UPDATE optinout_patients o SET o.i2b2_patient_num = pm.patient_num, o.i2b2_sync_time=NOW() ");
+			sql.append("  FROM patient_mapping pm");
+			sql.append(" WHERE o.pat_ref='PAT'");
+			sql.append("  AND o.pat_psn=pm.patient_ide");
+			sql.append("  AND pm.patient_ide_source=?"); // i2b2 project
+			args.add(prefs.get(PreferenceKey.i2b2Project));
+			break;
+		case Encounter:
+			sql.append("UPDATE optinout_patients o SET o.i2b2_patient_num = em.patient_num, o.i2b2_sync_time=NOW() ");
+			sql.append("  FROM encounter_mapping em");
+			sql.append(" WHERE o.pat_ref='ENC'");
+			sql.append("  AND o.pat_psn=em.encounter_ide");
+			sql.append("  AND pm.encounter_ide_source=?"); // i2b2 project
+			args.add(prefs.get(PreferenceKey.i2b2Project));
+			// TODO test what happens if more encounters are there for single patient
+		case Billing:
+			sql.append("UPDATE optinout_patients o SET o.i2b2_patient_num = f.patient_num, o.i2b2_sync_time=NOW() ");
+			sql.append("  FROM observation_fact f");
+			sql.append(" WHERE o.pat_ref='BIL'");
+			sql.append("  AND f.concept_cd='AKTIN:Fallkennzeichen'");// TODO use LOINC or other standard code for AKTIN:Fallkennzeichen
+			sql.append("  AND o.pat_psn=f.tval_char");
+			// no reference to i2b2 project
+			// TODO this will fail in cases where different patients use the same billing number (e.g. mother + child)
+		case Visit:
+			throw new UnsupportedOperationException("Not yet implemented"); // TODO use other number for visit identification
+		}
+		if( idRoot != null ) {
+			// limit to single patient
+			sql.append(" AND o.pat_psn=?");
+			String psn = anon.calculatePatientPseudonym(idRoot, idExt);
+			args.add( psn );
+		}
+		if( skipLinked == true ) {
+			// limit to unlinked patients
+			sql.append("  AND o.i2b2_patient_num IS NULL");
+		}
+		if( study != null ) {
+			// limit to single study
+			sql.append("  AND o.study_id=?");
+			args.add( study.getId() );
+		}
+		
+		PreparedStatement ps = dbc.prepareStatement(sql.toString());
+		// fill arguments
+		for( int i=0; i<args.size(); i++ ) {
+			ps.setString(i+1, args.get(i));
+		}
+		
+		return ps;
+	}
 	/**
 	 * Synchronize all entries with the specified reference type and study.
 	 * The {@code study} argument can be {@code null} to refer to all studies
-	 * @param ref reference type
-	 * @param study study, can be {@code null}
+	 * @param ref reference type, can be {@code null} to sync all types
+	 * @param study study to which the synchronization will be limited. Can be {@code null} to sync all studies.
+	 * @param skipLinked {@code false} to relink patients who were already linked previously. {@code true} to limit
+	 * linking to patients without previous link
 	 * @return completable future
 	 */
-	public CompletableFuture<SyncResult> syncBatch(PatientReference ref, Study study){
-		CompletableFuture<SyncResult> cf = new CompletableFuture<>();
-		
+	public CompletableFuture<SyncResult> syncBatch(PatientReference ref, Study study, boolean skipLinked) {
 		// TODO implement
+		return CompletableFuture.supplyAsync(() -> {
+			SyncResult result = new SyncResult();
+			try( Connection dbc = getConnection() ){
 
-		cf.completeExceptionally(new UnsupportedOperationException("Not yet implemented"));
-		return cf;
+				if( ref != null ) {
+					// run sync for single reference type
+					PreparedStatement ps = prepareSyncStatement(dbc, ref, study, skipLinked, null, null);
+					result.numLinked = ps.executeUpdate();
+					ps.close();
+				}else {
+					// run sync for all reference types
+					PreparedStatement ps;
+					ps = prepareSyncStatement(dbc, PatientReference.Patient, study, skipLinked, null, null);
+					result.numLinked += ps.executeUpdate();
+					ps.close();
+					ps = prepareSyncStatement(dbc, PatientReference.Encounter, study, skipLinked, null, null);
+					result.numLinked += ps.executeUpdate();
+					ps.close();
+					ps = prepareSyncStatement(dbc, PatientReference.Billing, study, skipLinked, null, null);
+					result.numLinked += ps.executeUpdate();
+					ps.close();
+				}
+				
+			} catch (SQLException e) {
+				throw new CompletionException(e);
+			}
+			return result;
+			
+		}, this.executor);
 	}
 
 }

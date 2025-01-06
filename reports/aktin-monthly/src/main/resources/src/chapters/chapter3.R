3cfbc15152da4cd205c2bd7bb635ff0486d40f9e
MarStreicher <mstreicher@uni-potsdam.de>
2025-01-06 09:52:43 +0100

    First refactoring of chapter3. This chapter includes procedures that are highly similar to those in chapter1; therefore, it would be possible to consolidate the logic into a single function.

--- reports/aktin-monthly/src/main/resources/src/chapters/chapter3.R
+++ reports/aktin-monthly/src/main/resources/src/chapters/chapter3.R
@@ -1,18 +1,67 @@
-#Transport and referral
-try({
-  table_formatted <- table(df$transport,useNA = "always")
-  names(table_formatted)[length(names(table_formatted))]<-'Keine Daten'
-  a <- table_formatted
-  b <- data.frame(Kategorie=names(a), Anzahl=format_number(a), Anteil=format_number((a / sum(a))*100,digits = 1))
-  c <- rbind(b, data.frame(Kategorie="Summe",Anzahl=format_number(sum(a)),Anteil=format_number(100,digits=1)))
-  c[,3] <- paste(c[,3],'%')
-  report_table(c,name='transport.xml',align=c('left','right','right'),widths=c(25,15,15))
-  
-  table_formatted <- table(df$referral,useNA = "always")
-  names(table_formatted)[length(names(table_formatted))]<-'Keine Daten'
-  a <- table_formatted
-  b <- data.frame(Kategorie=names(a), Anzahl=format_number(a), Anteil=format_number((a / sum(a))*100,digits = 1))
-  c <- rbind(b, data.frame(Kategorie="Summe",Anzahl=format_number(sum(a)),Anteil=format_number(100,digits=1)))
-  c[,3] <- paste(c[,3],'%')
-  report_table(c,name='referral.xml',align=c('left','right','right'),widths=c(60,15,15))
-}, silent=FALSE)
+# Transport and referral
+try(
+  {
+    ## Very similar to chapter1 - Patient Sex - Function?
+    table_formatted <- table(df$transport, useNA = "always")
+    names(table_formatted)[is.na(names(table_formatted))] <- "Keine Daten"
+
+    data_frame <- data.frame(
+      Category = names(table_formatted),
+      Count = as.numeric(table_formatted),
+      Percentage = as.numeric(table_formatted) / sum(table_formatted) * 100
+    )
+
+    data_frame <- rbind(
+      data_frame,
+      data.frame(
+        Category = "Summe",
+        Count = sum(table_formatted),
+        Percentage = sum(data_frame$Percentage)
+      )
+    )
+
+    data_frame$Count <- format_number(data_frame$Count)
+    data_frame$Percentage <- paste(format_number(data_frame$Percentage, digits = 1), "%")
+
+    report_table(
+      data_frame,
+      name = "transport_test.xml",
+      align = c("left", "right", "right"),
+      widths = c(25, 15, 15),
+      translations = column_name_translations
+    )
+    rm(table_formatted, data_frame)
+
+    ## Very similar to chapter1 - Patient Sex - Function?
+    table_formatted <- table(df$referral, useNA = "always")
+    names(table_formatted)[is.na(names(table_formatted))] <- "Keine Daten"
+
+    data_frame <- data.frame(
+      Category = names(table_formatted),
+      Count = as.numeric(table_formatted),
+      Percentage = as.numeric(table_formatted) / sum(table_formatted) * 100
+    )
+
+    data_frame <- rbind(
+      data_frame,
+      data.frame(
+        Category = "Summe",
+        Count = sum(table_formatted),
+        Percentage = sum(data_frame$Percentage)
+      )
+    )
+
+    data_frame$Count <- format_number(data_frame$Count)
+    data_frame$Percentage <- paste(format_number(data_frame$Percentage, digits = 1), "%")
+
+    report_table(
+      data_frame,
+      name = "refferal_test.xml",
+      align = c("left", "right", "right"),
+      widths = c(25, 15, 15),
+      translations = column_name_translations
+    )
+    rm(table_formatted, data_frame)
+  },
+  silent = FALSE
+)

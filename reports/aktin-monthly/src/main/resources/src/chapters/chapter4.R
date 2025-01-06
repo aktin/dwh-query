7ef1076f839efe654d526102f070dc2af28ba1e2
MarStreicher <mstreicher@uni-potsdam.de>
2025-01-06 10:44:54 +0100

    First refactoring of Chapter 4. This chapter also includes procedures that are very similar to those in Chapter 1; therefore, it would be possible to consolidate the logic into a single function

--- reports/aktin-monthly/src/main/resources/src/chapters/chapter4.R
+++ reports/aktin-monthly/src/main/resources/src/chapters/chapter4.R
@@ -1,23 +1,48 @@
-try({
-  table_formatted <- table(df$triage.result,useNA = "ifany") #NA is already mapped
-  a <- table_formatted
-  b <- data.frame(Kategorie=factors$Triage[!is.na(factors$Triage)], Anzahl=format_number(a), Anteil=format_number((a / sum(a))*100,digits = 1))
-  c <- rbind(b, data.frame(Kategorie="Summe",Anzahl=format_number(sum(a)),Anteil=format_number(100,digits=1)))
-  #c[,3] <- sprintf(fmt="%.1f",c[,3])
-  c[,3] <- paste(c[,3],'%')
-  report_table(c,'triage.xml',align=c('left','right','right'),widths=c(20,15,15))
-  
+try(
+  {
+    ## Very similar to chapter1 - Patient Sex - Function?
+    triage_res_summary <- table(df$triage.result, useNA = "ifany")
 
-  a<-data.frame(a)
-  graph<-ggplot(data=a, aes(x=Var1, y=Freq,fill=Var1)) +
-    geom_bar(stat="identity",position = "dodge", colour = "black",show.legend = FALSE)+
-    labs(x="Ersteinschätzung", y = "Anzahl Patienten")+
-    scale_fill_manual(values = c("red", "orange", "yellow2", "green4", "blue", "grey48"))+
-    theme(plot.caption = element_text(hjust=0.5,size=12),
-          panel.background = element_rect(fill = "white"),
-          axis.title = element_text(size=12),panel.border = element_blank(),axis.line = element_line(color = 'black'),
-          axis.text.x = element_text(face="bold", color="#000000", size=12),
-          axis.text.y = element_text(face="bold", color="#000000", size=12))+
-    scale_y_continuous(expand = c(0, 0.01))
-  report_svg(graph, 'triage')
-}, silent=FALSE)
+    data_frame <- data.frame(
+      Category = factors$Triage[!is.na(factors$Triage)],
+      Count = as.numeric(triage_res_summary),
+      Percentage = as.numeric(triage_res_summary) / sum(triage_res_summary) * 100
+    )
+
+    data_frame <- rbind(
+      data_frame,
+      data.frame(
+        Category = "Summe",
+        Count = sum(triage_res_summary),
+        Percentage = sum(data_frame$Percentage)
+      )
+    )
+
+    data_frame$Count <- format_number(data_frame$Count)
+    data_frame$Percentage <- paste(format_number(data_frame$Percentage, digits = 1), "%")
+
+    report_table(
+      data_frame,
+      "triage_test.xml",
+      align = c("left", "right", "right"),
+      widths = c(20, 15, 15),
+      translations = column_name_translations
+    )
+
+    graph <- ggplot(data = data.frame(triage_res_summary), aes(x = Var1, y = Freq, fill = Var1)) +
+      geom_bar(stat = "identity", position = "dodge", colour = "black", show.legend = FALSE) +
+      labs(x = "Ersteinschätzung", y = "Anzahl Patienten") +
+      scale_fill_manual(values = c("red", "orange", "yellow2", "green4", "blue", "grey48")) +
+      theme(
+        plot.caption = element_text(hjust = 0.5, size = 12),
+        panel.background = element_rect(fill = "white"),
+        axis.title = element_text(size = 12), panel.border = element_blank(), axis.line = element_line(color = "black"),
+        axis.text.x = element_text(face = "bold", color = "#000000", size = 12),
+        axis.text.y = element_text(face = "bold", color = "#000000", size = 12)
+      ) +
+      scale_y_continuous(expand = c(0, 0.01))
+    report_svg(graph, "triage_test")
+    rm(triage_res_summary, data_frame)
+  },
+  silent = FALSE
+)

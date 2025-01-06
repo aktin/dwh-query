1bd63a111c936db8a1c1de2ff60d9e1959e69dd4
MarStreicher <mstreicher@uni-potsdam.de>
2025-01-06 14:55:04 +0100

    Chapter6: Refactoring of discharge.xml

--- reports/aktin-monthly/src/main/resources/src/chapters/chapter6.R
+++ reports/aktin-monthly/src/main/resources/src/chapters/chapter6.R
@@ -1,26 +1,58 @@
-#Entlassung & Co
-try({
-  discharge_table <- table(df$discharge,useNA = "always")[c(14,7,1,2,3,6,5,4,11,9,13,10,8,12)]
-  names(discharge_table)[1]<-'Keine Daten'
-  discharge_table<-data.frame(discharge_table)
-  graph<-ggplot(data=discharge_table, aes(x=Var1, y=Freq)) +
-    geom_bar(stat="identity", fill="#046C9A",width = 0.5)+
-    labs(y="Anzahl Patienten", x = "")+
-    theme(plot.caption = element_text(hjust=0.5,size=12),
-          panel.background = element_rect(fill = "white"),
-          axis.title = element_text(size=12),panel.border = element_blank(),axis.line = element_line(color = 'black'),
-          axis.text.y = element_text(face="bold", color="#000000", size=12),
-          axis.text.x = element_text(face="bold", color="#000000", size=12))+
-    #scale_y_continuous(expand = c(0, 0.3))+
-    coord_flip()
-  report_svg(graph, 'discharge')
-}, silent=FALSE)
-try({
-  discharge_table <- table(df$discharge,useNA = "always")[c(12,8,10,13,9,11,4,5,6,3,2,1,7,14)] #reorder rows
-  names(discharge_table)[length(names(discharge_table))]<-'Keine Daten'
-  b <- data.frame(Kategorie=factors$Discharge[!is.na(factors$Discharge)], Anzahl=format_number(discharge_table), Anteil=format_number((discharge_table / sum(discharge_table))*100,digits = 1))
-  c <- rbind(b, data.frame(Kategorie="Summe",Anzahl=format_number(sum(discharge_table)),Anteil=format_number(100,digits=1)))
-  #c[,3] <- sprintf(fmt="%.1f",c[,3])
-  c[,3] <- paste(c[,3],'%')
-  report_table(c,name='discharge.xml',align=c('left','right','right'),widths=c(30,15,15))
-}, silent=FALSE)
+# Discharge
+try(
+  {
+    discharge_table <- table(df$discharge, useNA = "always")[c(14, 7, 1, 2, 3, 6, 5, 4, 11, 9, 13, 10, 8, 12)]
+    names(discharge_table)[1] <- "Keine Daten"
+    discharge_table <- data.frame(discharge_table)
+
+    graph <- ggplot(data = discharge_table, aes(x = Var1, y = Freq)) +
+      geom_bar(stat = "identity", fill = "#046C9A", width = 0.5) +
+      labs(y = "Anzahl Patienten", x = "") +
+      theme(
+        plot.caption = element_text(hjust = 0.5, size = 12),
+        panel.background = element_rect(fill = "white"),
+        axis.title = element_text(size = 12), panel.border = element_blank(), axis.line = element_line(color = "black"),
+        axis.text.y = element_text(face = "bold", color = "#000000", size = 12),
+        axis.text.x = element_text(face = "bold", color = "#000000", size = 12)
+      ) +
+      coord_flip()
+    report_svg(graph, "discharge")
+  },
+  silent = FALSE
+)
+
+## Very similar to chapter1 - Patient Sex - Function?
+try(
+  {
+    discharge_summary <- table(df$discharge, useNA = "always")[c(12, 8, 10, 13, 9, 11, 4, 5, 6, 3, 2, 1, 7, 14)]
+    names(discharge_summary)[is.na(names(discharge_summary))] <- "Keine Daten"
+
+    data_frame <- data.frame(
+      Category = names(discharge_summary),
+      Count = as.numeric(discharge_summary),
+      Percentage = as.numeric(discharge_summary) / sum(discharge_summary) * 100
+    )
+
+    data_frame <- rbind(
+      data_frame,
+      data.frame(
+        Category = "Summe",
+        Count = sum(discharge_summary),
+        Percentage = sum(data_frame$Percentage)
+      )
+    )
+
+    data_frame$Count <- format_number(data_frame$Count)
+    data_frame$Percentage <- paste(format_number(data_frame$Percentage, digits = 1), "%")
+
+    report_table(
+      data_frame,
+      name = "discharge.xml",
+      align = c("left", "right", "right"),
+      widths = c(25, 15, 15),
+      translations = column_name_translations
+    )
+    rm(discharge_summary, data_frame)
+  },
+  silent = FALSE
+)

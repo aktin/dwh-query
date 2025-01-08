b23b9729885b087c1c1c2c26d0b694542ca8fafa
MarStreicher <mstreicher@uni-potsdam.de>
2025-01-08 15:09:31 +0100

    Chapter9: Begin Refactoring last try block - not ready yet.

--- reports/aktin-monthly/src/main/resources/src/chapters/chapter9.R
+++ reports/aktin-monthly/src/main/resources/src/chapters/chapter9.R
@@ -108,86 +108,86 @@ try(
   silent = FALSE
 )
 
-# # Patienten in der NA
-# # Überfüllung
-# try(
-#   {
-#     a <- data.frame(
-#       admit.ts = as.POSIXct(df$admit.ts),
-#       discharge.ts = as.POSIXct(df$discharge.ts)
-#     )
-#     a <- na.omit(a)
-#     a <- a[order(as.Date(a$admit.ts)), ]
+# Patienten in der NA
+# Überfüllung
+try(
+  {
+    data_frame <- data.frame(
+      admit.ts = as.POSIXct(df$admit.ts),
+      discharge.ts = as.POSIXct(df$discharge.ts)
+    )
+    data_frame <- na.omit(data_frame)
+    data_frame <- data_frame[order(as.Date(data_frame$admit.ts)), ]
 
-#     a$in_na <- sapply(a$admit.ts, function(enter) {
-#       sum(a$admit.ts <= enter & a$discharge.ts > enter)
-#     })
+    data_frame$patients_in_emergency_room <- sapply(data_frame$admit.ts, function(enter) {
+      sum(data_frame$admit.ts <= enter & data_frame$discharge.ts > enter)
+    })
 
-#     init_last_month <- as.Date(a$admit.ts[1])
-#     end_last_month <- as.Date(a$admit.ts[nrow(a)])
+    init_last_month <- as.Date(data_frame$admit.ts[1])
+    end_last_month <- as.Date(data_frame$admit.ts[nrow(data_frame)])
 
-#     a$Uhrzeit <- as.numeric(format(a$admit.ts, "%H")) * 3600 +
-#       as.numeric(format(a$admit.ts, "%M")) * 60
-#     a$tag <- as.Date(a$admit.ts)
-#     a$tag_g <- format(a$admit.ts, format = "%a") # Weekday
-#     a$year <- as.numeric(format(a$admit.ts, "%Y"))
-#     a$day <- as.numeric(format(a$admit.ts, "%d"))
+    data_frame$Time <- as.numeric(format(data_frame$admit.ts, "%H")) * 3600 +
+      as.numeric(format(data_frame$admit.ts, "%M")) * 60
+    data_frame$Day_complete <- as.Date(data_frame$admit.ts)
+    data_frame$Week_day <- format(data_frame$admit.ts, format = "%a")
+    data_frame$Year <- as.numeric(format(data_frame$admit.ts, "%Y"))
+    data_frame$Day <- as.numeric(format(data_frame$admit.ts, "%d"))
+    data_frame$Week <- findInterval(data_frame$Day, seq(init_last_month, end_last_month, by = "week"))
 
-#     a$woche <- findInterval(a$tag, seq(init_last_month, end_last_month, by = "week"))
+    time_labels <- sprintf("%02d:00", seq(0, 24, by = 2))
 
-#     time_labels <- sprintf("%02d:00", seq(0, 24, by = 2))
+    missing <- data.frame(
+      Day = rep(seq(init_last_month, end_last_month, by = "day"), each = 2),
+      Time = c(0, 86400)
+    )
+    data_frame <- merge(data_frame, missing, by = c("Day", "Time"), all = TRUE)
 
-#     fehlend <- data.frame(
-#       tag = rep(seq(init_last_month, end_last_month, by = "day"), each = 2),
-#       Uhrzeit = c(0, 86400)
-#     )
-#     a <- merge(a, fehlend, by = c("tag", "Uhrzeit"), all = TRUE)
+###############
+    na_locf <- function(vec) {
+      for (i in seq_along(vec)) {
+        if (is.na(vec[i]) && i > 1) {
+          vec[i] <- vec[i - 1]
+        }
+      }
+      return(vec)
+    }
 
-#     na_locf <- function(vec) {
-#       for (i in seq_along(vec)) {
-#         if (is.na(vec[i]) && i > 1) {
-#           vec[i] <- vec[i - 1]
-#         }
-#       }
-#       return(vec)
-#     }
+    for (col in c("Week_day", "Day", "Week")) {
+      data_frame[[col]] <- na_locf(data_frame[[col]])
+    }
 
-#     for (col in c("tag_g", "day", "woche")) {
-#       a[[col]] <- na_locf(a[[col]])
-#     }
+    data_frame <- data_frame[-nrow(data_frame), ]
+    data_frame$Day <- format(as.Date(data_frame$Day), "%m-%d")
 
-#     a <- a[-nrow(a), ]
-#     a$tag <- format(as.Date(a$tag), "%m-%d")
+    weekly_titles <- sapply(1:5, function(i) {
+      start <- init_last_month + (i - 1) * 7
+      end <- pmin(init_last_month + i * 7 - 1, end_last_month)
+      paste0("Woche ", i, " ", start, " - ", end)
+    })
 
-#     weekly_titles <- sapply(1:5, function(i) {
-#       start <- init_last_month + (i - 1) * 7
-#       end <- pmin(init_last_month + i * 7 - 1, end_last_month)
-#       paste0("Woche ", i, " ", start, " - ", end)
-#     })
+    plot_week <- function(week_num, title, data_frame) {
+      data_frame_week <- subset(data_frame, Week == week_num)
+      data_frame_week <- subset(data_frame_week, !is.na(patients_in_emergency_room))
 
-#     plot_week <- function(week_num, title) {
-#       a_week <- subset(a, woche == week_num)
-#       a_week <- subset(a_week, !is.na(in_na))
+      ggplot(data = data_frame_week, aes(x = Time, y = patients_in_emergency_room, fill = Day, group = 1)) +
+        geom_line() +
+        facet_grid(Day + Week_day ~ .) +
+        theme_bw() +
+        xlab("Time") +
+        ylab("Anzahl Patienten") +
+        scale_x_continuous(
+          breaks = seq(0, 86400, by = 7200),
+          expand = c(0, 0),
+          labels = time_labels
+        ) +
+        coord_cartesian(xlim = c(0, 86400)) +
+        ggtitle(title)
+    }
 
-#       ggplot(data = a_week, aes(x = Uhrzeit, y = in_na, fill = tag, group = 1)) +
-#         geom_line() +
-#         facet_grid(tag + tag_g ~ .) +
-#         theme_bw() +
-#         xlab("Uhrzeit") +
-#         ylab("Anzahl Patienten") +
-#         scale_x_continuous(
-#           breaks = seq(0, 86400, by = 7200),
-#           expand = c(0, 0),
-#           labels = time_labels
-#         ) +
-#         coord_cartesian(xlim = c(0, 86400)) +
-#         ggtitle(title)
-#     }
-
-#     for (i in 1:5) {
-#       graph <- plot_week(i, weekly_titles[i])
-#       report_svg(graph, paste0("stayweek", i))
-#     }
-#   },
-#   silent = FALSE
-# )
+    for (i in 1:5) {
+      graph <- plot_week(i, weekly_titles[i], data_frame)
+      report_svg(graph, paste0("stayweek_test", i))
+    }
+  },
+  silent = FALSE
+)

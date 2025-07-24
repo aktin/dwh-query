7709d3f2d6ccf65af50219d3928003b4c04d33da
akomii <alexkombie@gmail.com>
2025-07-24 11:16:53 +0200

    added docs to P21ImportStats

--- generic-imports-manager/src/main/java/org/aktin/generic/imports/manager/P21ImportStats.java
+++ generic-imports-manager/src/main/java/org/aktin/generic/imports/manager/P21ImportStats.java
@@ -6,6 +6,13 @@ public class P21ImportStats {
   private final String source;
   private final int count;
 
+  /**
+   * Constructs import statistics with p21 encounter count for a given year and data source
+   *
+   * @param year   the year the data applies to
+   * @param source the csv source file of the data (mostly FALL, FAB, ICD or OPS)
+   * @param count  the number of found encounters in database
+   */
   public P21ImportStats(int year, String source, int count) {
     this.year = year;
     this.source = source;
